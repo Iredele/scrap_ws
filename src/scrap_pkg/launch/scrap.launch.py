63698145e133aa60b3dda203553554534c950f90
TheOneAboveAll <iredeledayo@yahoo.com>
2023-09-27 01:41:28 -0700

    Modified scrap launch file

--- src/scrap_pkg/launch/scrap.launch.py
+++ src/scrap_pkg/launch/scrap.launch.py
@@ -9,12 +9,34 @@ from ament_index_python import get_package_share_directory
 def generate_launch_description():
     ld = LaunchDescription()
     
+
+    motor_driver_node = Node(
+        package='scrap_pkg',
+        executable='motor_node'
+    )
+
+
     lidar_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('velodyne'),'launch',
                          'velodyne-all-nodes-VLP16-launch.py')
         )
     )
+
+    usb_cam_node = Node(
+            package='usb_cam',        
+            executable='usb_cam_node_exe'
+        )
+
+
+
+    ld.add_action(motor_driver_node)
+    ld.add_action(lidar_launch)
+    ld.add_action(usb_cam_node)
+    # ld.add_action(listener_node)
+    # ld.add_action(rsp_launch)
+    return ld
+
 
     # rsp_launch = IncludeLaunchDescription(
     #     PythonLaunchDescriptionSource(
@@ -23,25 +45,9 @@ def generate_launch_description():
     #     )
     # )
 
-    usb_cam_node = Node(
-            package='usb_cam',        
-            executable='usb_cam_node_exe'
-        )
-
-    motor_driver_node = Node(
-        package='scrap_pkg',
-        executable='motor_driver_node'
-    )
-
     # listener_node = Node(
     #     package='demo_nodes_py',
     #     executable='listener'
     # )
         
 
-    ld.add_action(lidar_launch)
-    # ld.add_action(rsp_launch)
-    ld.add_action(usb_cam_node)
-    ld.add_action(motor_driver_node)
-    # ld.add_action(listener_node)
-    return ld
