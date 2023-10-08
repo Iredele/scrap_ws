cfc64393a48b20dd837761a022e5401f4d640733
iredele <iredeledayo@yahoo.com>
2023-10-07 18:49:24 -0700

    added ros2 control

--- src/scrap_pkg/scrap_pkg/motor_node.py
+++ src/scrap_pkg/scrap_pkg/motor_node.py
@@ -14,11 +14,12 @@ class MotorDriver(Node):
             10
         )
 
-        self.left_motor_pin = 17
-        self.left_motor_dir = 18
-        self.right_motor_pin = 27
-        self.right_motor_dir = 22
+        self.left_motor_pin = 13    # black
+        self.left_motor_dir = 6     # yellow
+        self.right_motor_pin = 12   # grey
+        self.right_motor_dir = 5    # orange
 
+        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.left_motor_pin, GPIO.OUT)
         GPIO.setup(self.left_motor_dir, GPIO.OUT)
@@ -30,47 +31,44 @@ class MotorDriver(Node):
         self.left_motor_pwm.start(0)
         self.right_motor_pwm.start(0)
 
-def motor_callback(self, msg):
-    #Conver linear and angular velocities to motor speeds
-    linear_vel = msg.linear.x
-    angular_vel = msg.angular.z
+    def motor_callback(self, msg):
+        # Convert linear and angular velocities to motor speeds
+        linear_vel = msg.linear.x
+        angular_vel = msg.angular.z
 
-    #Calculate left and right wheel speeds
-    left_speed =  linear_vel - angular_vel
-    right_speed = linear_vel + angular_vel
+        # Calculate left and right wheel speeds
+        left_speed =  linear_vel - angular_vel
+        right_speed = linear_vel + angular_vel
 
+        # Determine direction (forward or reverse)
+        if left_speed >= 0:
+            left_direction = GPIO.HIGH
+        else:
+            left_direction = GPIO.LOW
 
-    #Determine direction (forward or reverse)
-    if left_speed >= 0:
-        left_direction = GPIO.HIGH
-    else:
-        left_direction = GPIO.LOW
+        if right_speed >= 0:
+            right_direction = GPIO.HIGH
+        else:
+            right_direction = GPIO.LOW
 
-    if right_speed >= 0:
-        right_direction = GPIO.HIGH
-    else:
-        right_direction = GPIO.LOW
+        # Set motor direction using GPIO pins
+        GPIO.output(self.left_motor_dir, left_direction)
+        GPIO.output(self.right_motor_dir, right_direction)
 
-    #Set motor direction using GPIO pins
-    GPIO.output(self.left_motor_dir, left_direction)
-    GPIO.output(self.right_motor_dir, right_direction)
+        # Scale motor speeds within PWM range (0-100%)
+        left_speed = max(min(abs(left_speed), 1.0), 0.0)
+        right_speed = max(min(abs(right_speed), 1.0), 0.0)
 
-    #Scale motor speeds within PWM range (0-100%)
-    left_speed = max(min(abs(left_speed), 1.0), 0.0)
-    right_speed = max(min(abs(right_speed), 1.0), 0.0)
+        # Map the scaled speeds to PWM range (0-100%)
+        left_pwm = int(left_speed * 100)
+        right_pwm = int(right_speed * 100)
 
-    #Map the scaled speeds to PWM range (0-100%)
-    left_pwm = int(left_speed * 100)
-    right_pwm = int(right_speed * 100)
+        self.get_logger().info(f'left pwm: {left_pwm}', once=False)
+        self.get_logger().info(f'right pwm: {right_pwm}', once=False)
 
-    # self.get_logger().info(f'left pwm: {left_pwm}', once=True)
-    # self.get_logger().info(f'right pwm: {right_pwm}', once=True)
-
-
-    #Set motor speeds using PWM
-    self.left_motor_pwm.ChangeDutyCycle(left_pwm)
-    self.right_motor_pwm.ChangeDutyCycle(right_pwm)
-
+        # Set motor speeds using PWM
+        self.left_motor_pwm.ChangeDutyCycle(left_pwm)
+        self.right_motor_pwm.ChangeDutyCycle(right_pwm)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -88,7 +86,7 @@ if __name__ == '__main__':
 
 # def main():
 #     print('Hi from scrap_pkg.')
-
+                                                        
 
 # if __name__ == '__main__':
 #     main()
