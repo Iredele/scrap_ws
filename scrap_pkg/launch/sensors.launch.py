d4700eececba526dda98b256918a92ed2fe02ff6
iredele <iredeledayo@yahoo.com>
2023-10-12 01:10:58 -0700

    added camera node

--- scrap_pkg/launch/sensors.launch.py
+++ scrap_pkg/launch/sensors.launch.py
@@ -16,6 +16,16 @@ def generate_launch_description():
         )
     )
 
+    usb_cam_node = Node(
+            package='v4l2_camera',        
+            executable='v4l2_camera_node',
+            output='screen',
+            parameters=[{
+                'image_size': [640, 480],
+                'camera_frame_id': 'camera_link_optical'
+            }]
+        )
+
     # rsp_launch = IncludeLaunchDescription(
     #     PythonLaunchDescriptionSource(
     #         os.path.join(get_package_share_directory('scrap_pkg'),'launch',
@@ -23,15 +33,12 @@ def generate_launch_description():
     #     )
     # )
 
-    usb_cam_node = Node(
-            package='usb_cam',        
-            executable='usb_cam_node_exe'
-        )
 
-    motor_driver_node = Node(
-        package='scrap_pkg',
-        executable='motor_driver_node'
-    )
+
+    # motor_driver_node = Node(
+    #     package='scrap_pkg',
+    #     executable='motor_driver_node'
+    # )
 
     # listener_node = Node(
     #     package='demo_nodes_py',
@@ -40,8 +47,8 @@ def generate_launch_description():
         
 
     ld.add_action(lidar_launch)
-    # ld.add_action(rsp_launch)
     ld.add_action(usb_cam_node)
-    ld.add_action(motor_driver_node)
+    # ld.add_action(rsp_launch)
+    #ld.add_action(motor_driver_node)
     # ld.add_action(listener_node)
     return ld
